9674163a404cc8feb463a81137826583aa3ade81
Andi Ahmad Yusup <andiahmadyusup@gmail.com>
2026-05-21 19:13:15 +0700

    feat (aircraft): add seats config for each aircraft

--- backend/app/Services/Aircraft/AircraftLayout.php
+++ backend/app/Services/Aircraft/AircraftLayout.php
@@ -7,8 +7,26 @@ class AircraftLayout
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    public function __construct(
+        public readonly string $type,
+        public readonly int $minRow,
+        public readonly int $maxRow,
+        public readonly array $seatLetters,
+    )
     {
         //
+    }
+
+    public function allSeats(): array
+    {
+        $seats = [];
+
+        for ($row = $this->minRow; $row <= $this->maxRow; $row++) {
+            foreach ($this->seatLetters as $letter) {
+                $seats[] = $row . $letter;
+            }
+        }
+
+        return $seats;
     }
 }
--- backend/config/aircraft.php
+++ backend/config/aircraft.php
@@ -1,5 +1,23 @@
 <?php
 
+
 return [
-    //
+    'layouts' => [
+
+        'ATR' => [
+            'rows' => ['min' => 1, 'max' => 18],
+            'seat_letters' => ['A', 'C', 'D', 'F'],
+        ],
+
+        'Airbus 320' => [
+            'rows' => ['min' => 1, 'max' => 32],
+            'seat_letters' => ['A', 'B', 'C', 'D', 'E', 'F'],
+        ],
+
+        'Boeing 737 Max' => [
+            'rows' => ['min' => 1, 'max' => 32],
+            'seat_letters' => ['A', 'B', 'C', 'D', 'E', 'F'],
+        ],
+
+    ],
 ];
