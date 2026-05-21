f14c9aff35b3a1434cebd4e0d73901e7df3aa473
Andi Ahmad Yusup <andiahmadyusup@gmail.com>
2026-05-21 19:10:16 +0700

    feat(voucher): add voucher model with fillable data

--- backend/app/Models/Voucher.php
+++ backend/app/Models/Voucher.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[Fillable(['crew_name', 'crew_id', 'flight_number', 'flight_date', 'aircraft_type', 'seat1', 'seat2', 'seat3'])]
 class Voucher extends Model
 {
     /** @use HasFactory<\Database\Factories\VoucherFactory> */
